447e7449495455897db50855bc43b264ea0699a7
somiltyagi <tyagi.somil1998@gmail.com>
2020-09-01 06:16:42 -0400

    new function of get current mac address added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
@@ -25,6 +26,23 @@ def mac_change(interface, mac_add):
     subprocess.call('ifconfig ' + interface + ' up ', shell=True)
 
 
-options = get_arguments()
+def current_mac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', interface]).decode('utf-8')
+    mac_address_search_result = re.search(r"..:..:..:..:..:..", ifconfig_result)
 
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+
+    else:
+        print('[-] Mac address could not found')
+
+
+options = get_arguments()
+cur_mac = current_mac(options.interface)
+print('CURRENT MAC = ' + str(cur_mac))
 mac_change(options.interface, options.mac_add)
+cur_mac = current_mac(options.interface)
+if cur_mac == options.mac_add:
+    print('[+] Mac address was successfully changed to' + cur_mac)
+else:
+    print('[-] Mac address could not be changed')
